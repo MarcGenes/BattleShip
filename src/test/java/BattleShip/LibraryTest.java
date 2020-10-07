e582e35d5f1931e0ba89df3338daea52bc303f15
Washin19 <washington.paredes@e-campus.uab.cat>
2020-10-07 13:03:36 +0200

    Clonado Repositorio

--- src/test/java/BattleShip/LibraryTest.java
+++ src/test/java/BattleShip/LibraryTest.java
@@ -3,8 +3,8 @@
  */
 package BattleShip;
 
-import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class LibraryTest {
     @Test public void testSomeLibraryMethod() {
