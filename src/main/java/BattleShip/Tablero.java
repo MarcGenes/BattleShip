2d9e31a4048adf6d1546f2725ff432bd87ba2730
marc <marc.genesca@e-campus.uab.cat>
2020-10-17 17:54:01 +0200

    creacion classe jugador i jugador test

--- src/main/java/BattleShip/Tablero.java
+++ src/main/java/BattleShip/Tablero.java
@@ -1,9 +1,14 @@
 package BattleShip;
 
 public class Tablero {
+	
+	private int size;
+	private boolean [][] tab;
+	
 		
 	public Tablero (int s) {
-		size=s;	
+		size=s;
+		tab = new boolean[size][size];
 	}
 	
 	public int getSize() {
@@ -11,6 +16,5 @@ public class Tablero {
 	}
 
 	
-	private int size;
 	
 }
